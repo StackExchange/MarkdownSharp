6439cb5980a8755215f7a3ecb3294098af90b1be
balpha <none@none>
2013-01-26 12:50:50 +0100

    unbreak everything (http://meta.stackoverflow.com/q/165096)

--- MarkdownSharp/Markdown.cs
+++ MarkdownSharp/Markdown.cs
@@ -1446,7 +1446,7 @@ namespace MarkdownSharp
             return Regex.Replace(match.Groups[1].Value, @"^  ", "", RegexOptions.Multiline);
         }
 
-        private static Regex _autolinkBare = new Regex(@"\b(https?|ftp)(://[-A-Z0-9+&@#/%?=~_|\[\]\(\)!:,\.;]*[-A-Z0-9+&@#/%=~_|\[\])])($|\W)",
+        private static Regex _autolinkBare = new Regex(@"(?<!<|="")\b(https?|ftp)(://[-A-Z0-9+&@#/%?=~_|\[\]\(\)!:,\.;]*[-A-Z0-9+&@#/%=~_|\[\])])($|\W)",
             RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         private static string handleTrailingParens(Match match)
